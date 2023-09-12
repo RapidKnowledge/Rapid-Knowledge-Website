d96f5f6ea72cb5c07ac3049970f58b49559e4da7
RajaPandian <rajapandian2003rp5555@gmail.com>
2023-09-12 21:25:31 +0530

    commit

--- src/Components/Appbar/Appbar.js
+++ src/Components/Appbar/Appbar.js
@@ -13,10 +13,10 @@ const Appbar = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
           <Nav>
-            <Nav.Link href="#home"><Link className="navtext" to="/">Home</Link></Nav.Link>
-            <Nav.Link href="#about"><Link className="navtext" to="/About">About</Link></Nav.Link>
-            <Nav.Link href="#explore"><Link className="navtext" to="/Explore">Explore</Link></Nav.Link>
-            <Nav.Link href="#contact"><Link className="navtext" to="/Contact">Contact</Link></Nav.Link>
+            <Nav.Link href="#home"><Link className="navtext" to="/"><i class="bi bi-house-fill"></i> Home</Link></Nav.Link>
+            <Nav.Link href="#about"><Link className="navtext" to="/About"><i class="bi bi-file-person"></i> About</Link></Nav.Link>
+            <Nav.Link href="#explore"><Link className="navtext" to="/Explore"><i class="bi bi-globe-asia-australia"></i> Explore</Link></Nav.Link>
+            <Nav.Link href="#contact"><Link className="navtext" to="/Contact"><i class="bi bi-person-lines-fill"></i> Contact</Link></Nav.Link>
             
           </Nav>
           
